d002d02b7f7ef521d66afa2dc124f4b53255253c
Amokke <24.02.1985@mail.ru>
2023-07-14 21:48:37 +0700

    Add Homework lesson4

--- src/main/java/org/example/lesson4/cw/task1/Calculat.java
+++ src/main/java/org/example/lesson4/cw/task1/Calculat.java
@@ -10,4 +10,41 @@ public class Calculat {
         }
         return sum;
     }
+    public double multi(List<? extends Number> numbers){
+        double result = 1.0;
+        for (Number number : numbers) {
+            result *= number.doubleValue();
+        }
+        return result;
+    }
+
+    public double divis(List<? extends Number> numbers) {
+        double result = 1.0;
+        for (Number number : numbers) {
+            result /= number.doubleValue();
+        }
+        return result;
+    }
+
+    public String binary(String strNum){
+        double number = Double.parseDouble(strNum);
+        int firstNum = (int) number;
+        double lastNum = number % firstNum;
+        int count = 0;
+        for (String s : strNum.split("")) {
+            if (s.charAt(0) == '.'){
+                count++;
+            }
+        }
+        if (count == 0) {
+            return Integer.toBinaryString(firstNum);
+        } else {
+            if (lastNum == 0){
+                return Integer.toBinaryString(firstNum);
+            }else {
+                return Integer.toBinaryString(firstNum) + "," +
+                        Long.toBinaryString(Double.doubleToRawLongBits(lastNum));
+            }
+        }
+    }
 }
